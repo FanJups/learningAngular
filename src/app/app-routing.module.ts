f16edd5542af6f22eab907878d41a6a5df742830
FanJups <jupsfan@gmail.com>
2019-12-10 14:57:57 +0100

    Working with Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component'; 
+import { ContactusComponent } from './contactus/contactus.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home", component:HomeComponent}, 
+  {path:"contactus", component:ContactusComponent} 
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const RoutingComponent = [HomeComponent,ContactusComponent];
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, RoutingComponent } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NewCmpComponent } from './new-cmp/new-cmp.component';
 import { ChangeTextDirective } from './change-text.directive';
 
 import { SqrtPipe } from './app.sqrt';
-import { HomeComponent } from './home/home.component';
-import { ContactusComponent } from './contactus/contactus.component';
+
 @NgModule({
   declarations: [
     SqrtPipe,
     AppComponent,
     NewCmpComponent,
     ChangeTextDirective,
-    HomeComponent,
-    ContactusComponent
+    RoutingComponent
   ],
   imports: [
     BrowserModule,
