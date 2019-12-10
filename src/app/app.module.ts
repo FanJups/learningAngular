9b6e1e76d5b6a44623ad3ce0d08a8a384f301a3d
FanJups <jupsfan@gmail.com>
2019-12-10 14:01:54 +0100

    Component Home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { NewCmpComponent } from './new-cmp/new-cmp.component';
 import { ChangeTextDirective } from './change-text.directive';
 
 import { SqrtPipe } from './app.sqrt';
+import { HomeComponent } from './home/home.component';
 @NgModule({
   declarations: [
     SqrtPipe,
     AppComponent,
     NewCmpComponent,
-    ChangeTextDirective
+    ChangeTextDirective,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
